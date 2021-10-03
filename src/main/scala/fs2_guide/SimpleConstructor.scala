ca00d5a78ad5a6834842c8cc668f4213e84402b8
kazuki <pharcydetip@gmail.com>
2021-10-03 16:50:12 +0900

    add SimpleConstructor

---
+++ src/main/scala/fs2_guide/SimpleConstructor.scala
@@ -0,0 +1,15 @@
+package fs2_guide
+import fs2.Stream
+
+object SimpleConstructor extends App {
+  // 空Streamの作成
+  println(Stream.empty.toList) // => List()
+
+  // ひとつの値からStreamを作成できる
+  println(Stream.emit(1).toList) // => List(1)
+
+  // 複数の値からStreamの作成
+  println(Stream.emits(List(1, 2)).toList)    // => List(1, 2)
+  println(Stream(1, 2).toList)                // => List(1, 2)
+  println(Stream.iterable(List(1, 2)).toList) // => List(1, 2)
+}
